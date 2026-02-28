0739527d0a77d6fc246a1b8bb005ed71e3427c4a
Shruti Natala <shrutinatala@gmail.com>
2026-02-28 15:47:55 -0800

    Initial Commit

---
+++ healthApp/healthAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  healthAppApp.swift
+//  healthApp
+//
+//  Created by Shruti Natala on 2/28/26.
+//
+
+import SwiftUI
+
+@main
+struct healthAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ healthAppTests/healthAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  healthAppTests.swift
+//  healthAppTests
+//
+//  Created by Shruti Natala on 2/28/26.
+//
+
+import Testing
+@testable import healthApp
+
+struct healthAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
