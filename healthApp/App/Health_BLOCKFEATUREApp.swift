dcb681326e8f6e0b0e4fc5f2a6a9fd5e8b877465
Kyle Yuen <45528039+kyleyuen@users.noreply.github.com>
2026-03-05 11:14:03 -0800

    Created the app window screen for blocking

---
+++ healthApp/App/Health_BLOCKFEATUREApp.swift
@@ -0,0 +1,17 @@
+//
+//  Health_BLOCKFEATUREApp.swift
+//  Health_BLOCKFEATURE
+//
+//  Created by Yuen Lok Hei Kyle on 1/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct Health_BLOCKFEATUREApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
